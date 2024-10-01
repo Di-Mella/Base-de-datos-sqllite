4b79659bbfad7a6230571d98c3403b803051875e
Tu Nombre <di.mella@duocuc.cl>
2024-10-01 14:15:01 -0300

    tercer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { ServicebdService } from '../services/servicebd.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomePage {
   nombre: string = "";
   valor: string = "";
 
-  constructor(private storage: NativeStorage) {}
+  constructor(private storage: NativeStorage, private bd:ServicebdService) {}
 
   crear(){
     this.storage.setItem(this.nombre, this.valor);
